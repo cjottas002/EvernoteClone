121cfd158d9c4e9c5e21ef67252210d8b3ef1894
cjottas_pc <joseortiz66@hotmail.com>
2024-12-15 16:10:56 +0100

    .

---
+++ EvernoteClone/View/UserControls/DisplayNoteBookControl.xaml.cs
@@ -0,0 +1,55 @@
+﻿using EvernoteClone.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvernoteClone.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNoteBookControl.xaml
+    /// </summary>
+    public partial class DisplayNoteBookControl : UserControl
+    {
+
+
+
+        public Notebook Notebook
+        {
+            get { return (Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(DisplayNoteBookControl), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DisplayNoteBookControl notebookUserControl)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+            }
+        }
+
+        public DisplayNoteBookControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EvernoteClone/View/UserControls/DisplayNoteControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using EvernoteClone.Model;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EvernoteClone.View.UserControls
+{
+
+    /// <summary>
+    /// Interaction logic for DisplayNoteControl.xaml
+    /// </summary>
+    public partial class DisplayNoteControl : UserControl
+    {
+        public Note Note
+        {
+            get { return (Note)GetValue(NoteProperty); }
+            set { SetValue(NoteProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoteProperty =
+            DependencyProperty.Register("Note", typeof(Note), typeof(DisplayNoteControl), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DisplayNoteControl noteUserControl)
+            {
+                noteUserControl.DataContext = noteUserControl.Note;
+            }
+        }
+        public DisplayNoteControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
